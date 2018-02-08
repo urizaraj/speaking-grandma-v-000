e887ae3e7006f81ae5cec751a769f753b406e480
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 21:57:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  shouting = phrase.all? do |letter|
-    letter == letter.upcase
-  end
-
-  puts shouting
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  if phrase == phrase.upcase
+    return NO, NOT SINCE 1938!
 end
-
-speak_to_grandma('hello')
